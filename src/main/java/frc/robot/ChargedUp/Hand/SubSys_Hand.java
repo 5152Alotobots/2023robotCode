ef207f6bd2a489fa82d384083454621f244bac00
SeanErn <seanernstes@gmail.com>
2023-01-22 16:56:32 -0500

    2023_01_22:17:00 Hand System

---
+++ src/main/java/frc/robot/ChargedUp/Hand/SubSys_Hand.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.ChargedUp.Hand;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SubSys_Hand extends SubsystemBase {
+  /** Creates a new SubSys_Hand. */
+  public SubSys_Hand() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public final Solenoid m_handSolenoid = new Solenoid(
+    PneumaticsModuleType.CTREPCM, 
+    SubSys_Hand_Constants.kHandSolenoidChannel
+  );
+
+}
---
+++ src/main/java/frc/robot/Library/Pneumatics/SubSys_Pneumatics_Constants.java
@@ -0,0 +1,5 @@
+package frc.robot.Library.Pneumatics;
+
+public class SubSys_Pneumatics_Constants {
+    
+}
