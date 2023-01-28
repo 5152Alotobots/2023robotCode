ebe31daf55de2039499214c8e6f2adc6139e1a73
Chris DeBoer <45087060+chrisdeboer@users.noreply.github.com>
2023-01-28 12:12:29 -0500

    Added Color Sensor

---
+++ src/main/java/frc/robot/ChargedUp/ColorSensor/Subsys_ColorSensor.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.ChargedUp.ColorSensor;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
+
+public class Subsys_ColorSensor extends SubsystemBase {
+  /** Creates a new Subsys_ColorSensor. */
+  public Subsys_ColorSensor() {}
+
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  
+
+  
+  
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+
+      Color detectedColor = m_colorSensor.getColor();
+      
+      double IR = m_colorSensor.getIR();
+
+      SmartDashboard.putNumber("Red", detectedColor.red);
+      SmartDashboard.putNumber("Green", detectedColor.green);
+      SmartDashboard.putNumber("Blue", detectedColor.blue);
+      SmartDashboard.putNumber("IR", IR);
+
+  }
+}
---
+++ src/main/java/frc/robot/ChargedUp/DistanceSensor/SubSys_DistanceSensor.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.ChargedUp.DistanceSensor;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.*;
+
+
+public class SubSys_DistanceSensor extends SubsystemBase {
+  /** Creates a new SubSys_DistanceSensor. */
+  public SubSys_DistanceSensor() {}
+  // private Rev2mDistanceSensor distSens;
+  // distSens = new Rev2mDistanceSensor(Port.kOnboard);
+ 
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
