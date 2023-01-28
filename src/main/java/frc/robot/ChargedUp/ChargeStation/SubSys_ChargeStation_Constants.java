d08f6973540174974590d2ba334b994f0ffe5cc4
BuildTools <unconfigured@null.spigotmc.org>
2023-01-27 22:52:02 -0500

    add potentail auto balance code

---
+++ src/main/java/frc/robot/ChargedUp/ChargeStation/SubSys_ChargeStation_Constants.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.ChargedUp.ChargeStation;
+
+/** Add your docs here. */
+public class SubSys_ChargeStation_Constants {
+//Max speed of the robot while autonomously balancing
+public static final double adjustForwardSpeed = 0.25;
+public static final double adjustBackwardSpeed = -0.25;
+public static final double adjustLeftSpeed = 0.25;
+public static final double adjustRightSpeed = -0.25;
+}
